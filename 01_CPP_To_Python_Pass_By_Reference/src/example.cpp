2ba6c24d1657090dbba7782e34284b73c07c2174
Vincent Paladino <vincent.paladino@zebra.com>
2019-09-24 09:01:34 -0400

    Added Pass-By-Reference example and .gitignore

---
+++ 01_CPP_To_Python_Pass_By_Reference/src/example.cpp
@@ -0,0 +1,48 @@
+#include <pybind11/embed.h>
+#include <pybind11/stl_bind.h>
+#include <iostream>
+
+// [1] Must be placed at the top of the file, directly after #includes
+PYBIND11_MAKE_OPAQUE(std::vector<int>);
+
+namespace py = pybind11;
+using namespace py::literals;
+
+/* [2] Specifies the binary module. This can then be imported
+ *  in the python script
+ */
+PYBIND11_EMBEDDED_MODULE(examplemodule, m) 
+{
+    py::bind_vector<std::vector<int>>(m, "ByteVector");
+}
+
+int main(int argc, char const *argv[])
+{
+    py::scoped_interpreter guard{};
+
+    // Instantiate vector and fill with data
+    std::vector<int> exampleData;
+    exampleData.push_back(1);
+
+    auto module = py::module::import("examplemodule");
+    
+    // Pass exampleData object to python script
+    auto locals = py::dict(
+        "example_data"_a=py::cast(exampleData, py::return_value_policy::reference), // by reference
+        **module.attr("__dict__"));
+
+    std::cout << "exampleData[0] value C++: " << exampleData[0] << '\n';
+
+    py::eval_file("example.py", py::globals(), locals);
+    
+    std::cout << "exampleData[0] value C++ after running example.py: " << exampleData[0] << '\n';
+
+    return 0;
+}
+
+/** Pybind11 Relevant documentation links:
+ * [1] https://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html#binding-stl-containers
+ * [2] https://pybind11.readthedocs.io/en/stable/advanced/embedding.html#adding-embedded-modules
+ * 
+ */
+
---
+++ 01_CPP_To_Python_Pass_By_Reference/src/example.py
@@ -0,0 +1,9 @@
+import examplemodule
+
+print("************Python Example File Script Start************")
+
+print("example_data[0] original value Python: " + str(example_data[0]))
+example_data[0] = 10
+print("example_data[0] newmodified in Python: " +  str(example_data[0]))
+
+print("************Python Example File Script Finish************")
